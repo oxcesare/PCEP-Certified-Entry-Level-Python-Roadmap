647bb3984dc81417b3bbea446ef1f46c8175359c
Cesar <cesar180687@hotmail.com>
2026-05-09 18:50:59 -0600

    Ejercicios Básicos Certificación Pyhton: Operadores

--- com/mx/curso/basics/elementos.py
+++ com/mx/curso/basics/elementos.py
@@ -1,1 +1,17 @@
 print("Elementos basicos de Python")
+
+def tiposDatos():
+    print("tipos de datos")
+    a=1
+    b =1.2
+    c ='a'
+    print(type(a))
+    print(type(b))
+    print(type(c))
+
+def main():
+     tiposDatos()
+
+if __name__ == "__main__":
+    main()
+
---
+++ com/mx/curso/basics/operadores.py
@@ -0,0 +1,22 @@
+""""
+Ejemplo del uso de Operadores en Python
+"""
+
+def demoOperadores():
+    a = 10+20
+    b = 20-10
+    c = 30/5
+    d = 4.5*9.5
+    #Ejemplo del uso del operador %
+    e = 3%0
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+def main():
+    demoOperadores()
+
+if __name__ == "__main__":
+    main()
